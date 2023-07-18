bfe248b38e3fa75fb25691b21aa0f6a308804fd4
Muhammed Üşenmez <59093336+musenmez@users.noreply.github.com>
2023-07-18 11:51:53 +0300

    Add ToppingBottle.cs

---
+++ Assets/[Game]/Scripts/Topping/ToppingBottle.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Runtime 
+{
+    public class ToppingBottle : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            
+        }
+
+        private void OnDisable()
+        {
+            
+        }
+    }
+}
