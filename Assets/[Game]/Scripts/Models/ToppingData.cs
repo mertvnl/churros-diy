3a1860393ca5c78112ab5e485d3dccf99ca98607
Muhammed Üşenmez <59093336+musenmez@users.noreply.github.com>
2023-07-18 12:14:12 +0300

    Add ToppingData.cs

---
+++ Assets/[Game]/Scripts/Models/ToppingData.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Models 
+{
+    [CreateAssetMenu(fileName = "Topping Data", menuName = "Scriptable Objects/Create ToppingData")]
+    public class ToppingData : ScriptableObject
+    {
+        public string Name;
+        public Sprite Icon;
+        public List<GameObject> Prefabs;
+    }
+}
+
